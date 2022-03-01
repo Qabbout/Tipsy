06ac9df1978f5cf4b4b5e5ed828d8ebc579516b7
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2022-03-01 19:08:43 +0200

    add ResultsViewController

---
+++ Tipsy/ResultsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Abdulrahman on 3/1/22.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recalculateBtnPressed(_ sender: UIButton) {
+    }
+}
