a4bc5c9202e0d41d7bac0f67c9671e4199fcdcd8
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2022-03-01 19:24:08 +0200

    added tipChanged functionality

--- Tipsy/CalculatorViewController.swift
+++ Tipsy/CalculatorViewController.swift
@@ -17,8 +17,25 @@ class CalculatorViewController: UIViewController {
     
     @IBOutlet weak var splitNumberLabel: UILabel!
     
+    var tip = 0.10
+    var numberOfPeople = 2
+    var billTotal = 0.0
+    var finalResult = "0.0"
     
     @IBAction func tipChanged(_ sender: UIButton) {
+        
+        billTextField.endEditing(true)
+        
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        sender.isSelected = true
+        
+        
+        let buttonTitleMinusPercentSign =  String(sender.currentTitle!.dropLast())
+        let buttonTitleAsANumber = Double(buttonTitleMinusPercentSign)!
+        tip = buttonTitleAsANumber / 100
+        
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
