aa02630160ac3ff3b3aea845211826864dccecd1
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2022-03-01 19:04:32 +0200

    renamed viewController to CalculatorViewController

--- Tipsy/CalculatorViewController.swift
+++ Tipsy/CalculatorViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CalculatorViewController.swift
 //  Tipsy
 //
 //  Created by Abdulrahman on 2/26/22.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalculatorViewController: UIViewController {
 
     @IBOutlet weak var billTextField: UITextField!
     
